efd0ff953c3286780f8dda44b1c479a292447bb0
jashimuddin211 <jashm211@gmail.com>
2026-03-01 10:11:03 +0600

    serives card done

--- src/component/pages/Services/Services.jsx
+++ src/component/pages/Services/Services.jsx
@@ -18,11 +18,26 @@ const Services = () => {
   }, []);
    
     return (
-        <div>
-            <div>
+        <div className='max-w-[1280px] mx-auto'>
+            <div className='grid grid-cols-3 gap-5'>
                 {
                     data.map(d => (
-                        <div>{d.serviceName}</div>
+                        // <div>{d.serviceName}</div>
+                        <div className="card bg-base-100 w-96 shadow-sm">
+  <figure >
+    <img
+    className='max-w-80 max-h-60 p-4'
+      src={d.image}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">{d.serviceName}</h2>
+    <p className='text-amber-600 font-bold '>{d.category}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
                     ))
                 }
             </div>
